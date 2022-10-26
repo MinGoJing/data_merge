1795a41acb76250dd6ea8fc53d0d89d094cc4894
git_user <mingo_jing@163.com>
2022-10-27 07:42:38 +0800

    [Initial] cm

---
+++ utils/string.py
@@ -0,0 +1,20 @@
+from email import header
+
+
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   string.py
+@Desc    :   provide string support method or class
+@Version :   1.0
+@Author  :   MinGo
+@Contact :   mingo_jing@163.com
+@License :   Copyright © since 2022, MinGo
+@History :   
+    1.0: 2022/10/26 17:37, MinGo
+          1. Created.
+
+'''
+
+# py
+
